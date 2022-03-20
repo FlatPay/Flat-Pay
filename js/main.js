9d79dd9ad633fbfedf115dc36e910a0b80aea89d
FlatPay <flatpayweb@gmail.com>
2022-03-20 15:42:12 +0000

    new

--- js/main.js
+++ js/main.js
@@ -3,4 +3,20 @@ jQuery(document).ready(function( ){
 
  new WOW().init();
 
+
+ 
+    $('.dropright button').on("click", function(e) {
+      e.stopPropagation();
+      e.preventDefault();
+  
+      if (!$(this).next('div').hasClass('show')) {
+        $(this).next('div').addClass('show');
+      } else {
+        $(this).next('div').removeClass('show');
+      }
+  
+    });
+  
+
   });
+
